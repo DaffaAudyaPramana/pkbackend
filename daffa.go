78c96852275c362f69b5689367c51ccfde40b596
Mystlicious <Daffaaudyapramana03@gmail.com>
2023-03-24 20:47:01 +0700

    update

--- daffa.go
+++ daffa.go
@@ -4,10 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -29,63 +26,38 @@ func InsertOneDoc(db string, collection string, doc interface{}) (insertedID int
 	}
 	return insertResult.InsertedID
 }
-
-func InsertUser(nm string, gnr string, eml string, nhp string) (InsertedID interface{}) {
-	var user User
-	user.Nama = nm
-	user.Gender = gnr
-	user.Email = eml
-	user.NoHp = nhp
-
-	return InsertOneDoc("dbuser", "user", user)
-}
-
-func InsertPembayaran(sts string, tbr string) (InsertedID interface{}) {
-	var pembayaran Pembayaran
-	pembayaran.Datetime = primitive.NewDateTimeFromTime(time.Now().UTC())
-	pembayaran.Status = sts
-	pembayaran.TotalBayar = tbr
-
-	return InsertOneDoc("dbpembayaran", "pembayaran", pembayaran)
-}
-
-func InsertPendaftaran(nis string, nik string, nsa string) (InsertedID interface{}) {
-	var pendaftaran Pendaftaran
-	pendaftaran.Nis = nis
-	pendaftaran.Nik = nik
-	pendaftaran.NamaSiswa = nsa
-
-	return InsertOneDoc("dbpendaftaran", "pendaftaran", pendaftaran)
-}
-
-func InsertPengumuman(hsi string, nli string, pgm string) (InsertedID interface{}) {
-	var pengumuman Pengumuman
-	pengumuman.HasilSeleksi = hsi
-	pengumuman.Nilai = nli
-	pengumuman.Program = pgm
-
-	return InsertOneDoc("dbpengumuman", "pengumuman", pengumuman)
-}
-
-func InsertProgramKursus(nks string, jks string, pgr string) (InsertedID interface{}) {
-	var programkursus ProgramKursus
-	programkursus.NamaKursus = nks
-	programkursus.JenjangKursus = jks
-	programkursus.Pengajar = pgr
-
-	return InsertOneDoc("dbprogramkursus", "programkursus", programkursus)
-}
-
-func GetDataUser(stats string) (data []User) {
-	user := MongoConnect("dbuser").Collection("user")
-	filter := bson.M{"nama": stats}
-	cursor, err := user.Find(context.TODO(), filter)
+func InsertUser(db string, user User) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("user").InsertOne(context.TODO(), user)
 	if err != nil {
-		fmt.Println("GetDataUser :", err)
+		fmt.Printf("InsertUser: %v\n", err)
 	}
-	err = cursor.All(context.TODO(), &data)
+	return insertResult.InsertedID
+}
+func InsertPendaftaran(db string, pendaftaran Pendaftaran) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("pendaftaran").InsertOne(context.TODO(), pendaftaran)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("InsertPendaftaran: %v\n", err)
 	}
-	return
+	return insertResult.InsertedID
+}
+func InsertPembayaran(db string, pembayaran Pembayaran) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("pembayaran").InsertOne(context.TODO(), pembayaran)
+	if err != nil {
+		fmt.Printf("InsertPembayaran: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+func InsertPengumuman(db string, pengumuman Pengumuman) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("pengumuman").InsertOne(context.TODO(), pengumuman)
+	if err != nil {
+		fmt.Printf("InsertPengumuman: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+func InsertKursus(db string, kursus Kursus) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection("kursus").InsertOne(context.TODO(), kursus)
+	if err != nil {
+		fmt.Printf("InsertKursus: %v\n", err)
+	}
+	return insertResult.InsertedID
 }
--- daffa_test.go
+++ daffa_test.go
@@ -1,76 +1,74 @@
 package pkbackend
 
 import (
-	"fmt"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// func TestInsertUser(t *testing.T) {
-// 	nm := "Daffa Audya Pramana"
-// 	gnr := "1214015"
-// 	eml := "Daffaaudyapramana03@gmail.com"
-// 	nhp := "085722374028"
-// 	hasil := InsertUser(nm, gnr, eml, nhp)
-// 	fmt.Println(hasil)
-
-// }
-
-// func TestPembayaran(t *testing.T) {
-// 	sts := "Dibayar"
-// 	tbr := "200,000"
-// 	hasil := InsertPembayaran(sts, tbr)
-// 	fmt.Println(hasil)
-// }
-
-// func TestPendaftaran(t *testing.T) {
-// 	nis := "92348927348"
-// 	nik := "72619873972094293"
-// 	nsa := "Daffa Audya Pramana"
-// 	hasil := InsertPendaftaran(nis, nik, nsa)
-// 	fmt.Println(hasil)
-// }
-
-// func TestInsertPengumuman(t *testing.T) {
-// 	hsi := "LULUS"
-// 	nli := "97"
-// 	pgm := "Fighting"
-// 	hasil := InsertPengumuman(hsi, nli, pgm)
-// 	fmt.Println(hasil)
-// }
-
-// func TestInsertProgramKursus(t *testing.T) {
-// 	nks := "FIGHTING GAME"
-// 	jks := "Program 3"
-// 	pgr := "Budi Setiawan"
-// 	hasil := InsertProgramKursus(nks, jks, pgr)
-// 	fmt.Println(hasil)
-// }
-
-func TestGetDataUser(t *testing.T) {
-	stats := "Daffa Audya Pramana"
-	data := GetDataUser(stats)
-	fmt.Println(data)
+func TestInsertUser(t *testing.T) {
+	dbname := "proyek-2"
+	user := User{
+		ID:     primitive.NewObjectID(),
+		Nama:   "Arya",
+		Gender: "Perempuan",
+		Email:  "Arya@gmail.com",
+	}
+	insertedID := InsertUser(dbname, user)
+	if insertedID == nil {
+		t.Error("Failed to insert user")
+	}
 }
 
-// func TestGetDataAbout(t *testing.T) {
-// 	stats := "Apakah kurikulum ini menyulitkan hidup?"
-// 	data := GetDataAbout(stats)
-// 	fmt.Println(data)
-// }
+func TestInsertPendaftaran(t *testing.T) {
+	dbname := "proyek-2"
+	pendaftaran := Pendaftaran{
+		ID:         primitive.NewObjectID(),
+		Nama_siswa: "Arya",
+		Nis:        "92348927348",
+		Nik:        "72619873972094293",
+	}
+	insertedID := InsertPendaftaran(dbname, pendaftaran)
+	if insertedID == nil {
+		t.Error("Failed to insert pendaftaran")
+	}
+}
 
-// func TestGetDataContacus(t *testing.T) {
-// 	stats := "0822126722"
-// 	data := GetDataContacus(stats)
-// 	fmt.Println(data)
-// }
-
-// func TestGetDataDashboard(t *testing.T) {
-// 	stats := "Denmark"
-// 	data := GetDataDashboard(stats)
-// 	fmt.Println(data)
-// }
-// func TestGetDataMahasiswa(t *testing.T) {
-// 	stats := "johndoe@flex.co"
-// 	data := GetDataMahasiswa(stats)
-// 	fmt.Println(data)
-// }
+func TestInsertPembayaran(t *testing.T) {
+	dbname := "proyek-2"
+	pembayaran := Pembayaran{
+		ID:          primitive.NewObjectID(),
+		Status:      "92348927348",
+		Total_bayar: "72619873972094293",
+	}
+	insertedID := InsertPembayaran(dbname, pembayaran)
+	if insertedID == nil {
+		t.Error("Failed to insert pembayaran")
+	}
+}
+func TestInsertPengumuman(t *testing.T) {
+	dbname := "proyek-2"
+	pengumuman := Pengumuman{
+		ID:            primitive.NewObjectID(),
+		Hasil_seleksi: "LULUS",
+		Nilai:         "97",
+		Program:       "Fighting",
+	}
+	insertedID := InsertPengumuman(dbname, pengumuman)
+	if insertedID == nil {
+		t.Error("Failed to insert pengumuman")
+	}
+}
+func TestInsertKursus(t *testing.T) {
+	dbname := "proyek-2"
+	kursus := Kursus{
+		ID:             primitive.NewObjectID(),
+		Nama_kursus:    "FIGHTING GAME",
+		Jenjang_kursus: "Program 3",
+		Pengajar:       "Jon Snow",
+	}
+	insertedID := InsertKursus(dbname, kursus)
+	if insertedID == nil {
+		t.Error("Failed to insert kursus")
+	}
+}
